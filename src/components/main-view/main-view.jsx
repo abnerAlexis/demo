526edc65a750aa1c0f0e6d45bcc0ef196947d2dd
Alexis <68366562+abnerAlexis@users.noreply.github.com>
2024-01-26 18:46:46 -0700

    Routing (#7)
    * New branch 37R, install react-router and react-router-dom
    
    * Import 'BrowserRouter', 'Routes', 'Route', and 'Navigate' and used them in mainview.jsx
    
    * Code cleaning
    
    * Link element added to Card component in BookCard component
    
    * Import usoParams and Link to book-view.jsx, update button using Link
    
    * Code lint and fix in book-view
    
    * Create navigation-bar folder and navigation-bar.jsx file in it
    
    * Create Navbar component
    
    * Pass user and onLoggedOut props to <NavigationBar>
    
    * Update index.scss and navbar theme to be dark
    
    ---------
    
    Co-authored-by: Alexis Abner <alexisabner@alexiss-air.lan>

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,13 +3,13 @@ import { BookCard } from "../book-card/book-card";
 import { BookView } from "../book-view/book-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
-
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap"
+import { NavigationBar } from "../navigation-bar/navigation-bar";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
   const [books, setBooks] = useState([]);
-  const [selectedBook, setSelectedBook] = useState(null);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -21,7 +21,7 @@ export const MainView = () => {
             id: doc.key,
             title: doc.title,
             image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-            author: doc.author_name?.[0]
+            author: doc.author_name?.[0],
           };
         });
 
@@ -29,7 +29,88 @@ export const MainView = () => {
       });
   }, []);
 
-    return (
+  return (
+    <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+        }}
+      />
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <SignupView />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/books/:bookId"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : books.length === 0 ? (
+                  <Col>There are no books to show.</Col>
+                ) : (
+                  <Col md={8}>
+                    <BookView books={books} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : books.length === 0 ? (
+                  <Col>There are no books to show.</Col>
+                ) : (
+                  <>
+                    {books.map((book) => (
+                      <Col className="mb-4" key={book.id} md={3}>
+                        <BookCard book={book} />
+                      </Col>
+                    ))}
+                  </>
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
+  );
+};
+/*
+
+  return (
       <Row className="justify-content-md-center">
         {!user ? (
           <Col md={5} style={{border: "2px solid black"}}>
@@ -64,4 +145,5 @@ export const MainView = () => {
         )}
       </Row>
     );
-};
+
+*/
